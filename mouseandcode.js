a621687b97e5b78f39b172f72bec46de8ab90605
macosta2015 <macosta2015@my.fit.edu>
2025-01-30 23:52:49 -0500

    Update mouseandcode.js

--- mouseandcode.js
+++ mouseandcode.js
@@ -267,8 +267,8 @@ require('events').EventEmitter.defaultMaxListeners = 20; // Set the maximum numb
         //Working on Extrude 1
         ////////////////////////////////////////////////////////////////////////////
         const selector11 = 'div[data-id="gdrROUTBVHLLvba1"]'; // Replace with the appropriate selector
-        const title11 = 'Extrude 1 did not regenerate properly: Select face or sketch region to extrude.<br>2 missing selections';
-        // const title11 = 'Extrude 1 did not regenerate properly: Select face or sketch region to extrude.'; //Test with this one, make sure to comment above
+        // const title11 = 'Extrude 1 did not regenerate properly: Select face or sketch region to extrude.<br>2 missing selections';
+        const title11 = 'Extrude 1 did not regenerate properly: Select face or sketch region to extrude.'; //Test with this one, make sure to comment above
         const editOptions11 = await performRightClickOptionByTitle(newPage, selector11, title11);
         console.log("JJJJJJJJJJJ");
         console.log(''); // This logs an empty line, effectively skipping a line
